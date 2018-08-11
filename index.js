054ee59e8462a453da11c28ce366405ff084aaf0
Ddone <mynameisthenekit@gmail.com>
2018-08-11 11:31:38 +0300

    trying https

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const https = require('https');
+const fs = require('fs')
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = process.env.TOKEN || "590456891:a"
+
+
+const PORT = process.env.PORT || 5000
+const TOKEN = process.env.TOKEN || "590456891:AAEkOXQo2UuYnIw1wnLyKrbKgNyGnJigqbQ"
 
 console.log(PORT,TOKEN);
+let bot = 0
 
 
 
-const bot = new TelegramBot(TOKEN)
+if("HEROKU" in process.env){
+  console.log("on heroku")
+  https.createServer({}, (req, res) => {
+    res.writeHead(200);
+    res.end('hello world\n');
+  }).listen(8000);
+  bot = new TelegramBot(TOKEN,{polling:true})
 
+
+}else{
+  bot = new TelegramBot(TOKEN,{polling:true})
+}
+
+bot.on("message",mes =>{
+  console.log(mes)
+})
 
 bot.getMe().then(val => console.log(val))
